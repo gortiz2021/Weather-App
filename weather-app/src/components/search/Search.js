f5370960d25090e9dd03995d233f847c6db91c9f
Gabriel Ortiz <igabriel661@hotmail.com>
2023-04-22 14:56:42 -0700

    Got the api to work. Next is to add the weekly forecast.

--- weather-app/src/components/search/Search.js
+++ weather-app/src/components/search/Search.js
@@ -1,17 +1,75 @@
-import React from 'react';
+import React, { useState} from 'react';
 import './Search.css';
-import { TextField } from '@mui/material';
+import { TextField, Typography, Button } from '@mui/material';
 
 const Search = () => {
+
+  const [longitude, setLongitude] = useState('');
+  const [latitude, setLatitude] = useState('');
+  const [city, setCity] = useState('');
+  const [cityName, setCityName] = useState('');
+  const [weather, setWeather] = useState(null);
+  
+  const API_KEY = '46654978ae9bab68acd43f8439cbcac5';
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const LOCATION_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`
+    
+    try { 
+      const response = await fetch(LOCATION_URL);
+      const data  = await response.json();
+
+      if (!data || !data[0]) {
+        // Handle the case where data is undefined or the array is empty
+        console.error(`No data returned for ${city}`);
+        return;
+      }
+
+      console.log(data);
+      setCityName(data[0].local_names.en);
+      setLatitude(data[0].lat);
+      setLongitude(data[0].lon);
+
+      const WEATHER_URL = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&units=imperial&&appid=${API_KEY}`
+
+      const weatherResponse = await fetch(WEATHER_URL);
+      const weatherData = await weatherResponse.json();
+      console.log(weatherData);
+      setWeather(weatherData);
+    }
+    catch (e) { 
+      console.log(`Error fetching data: ${e}`);
+    }  
+  }
+
   return (
-    <div className='search-container'>
+    <div>
+      <Typography variant='h1'>Weather App</Typography>
+      <form onSubmit={handleSubmit}>
         <TextField 
-            id='search-bar'
-            label='search'
-            varient='standard'
+          label="Enter city"
+          variant="outlined"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
         />
+        <Button variant="contained" color="primary" type="submit">
+          Get Weather
+        </Button>
+      </form>
+
+    {weather && (
+      <div>
+        <Typography variant="h2">{cityName}</Typography>
+        <Typography varient='body1'>{weather.current.temp}°F</Typography>
+        <Typography varient='body1'>{weather.current.weather[0].main}</Typography>
+      </div> 
+    )}
+
+
     </div>
-  )
-}
+  );
+};
+
 
 export default Search;
