d4265b75be6bc76d987ed0c3ac42756642c28e50
Gabriel Ortiz <igabriel661@hotmail.com>
2023-05-11 11:12:24 -0700

    Added comments to search.js

--- weather-app/src/components/search/Search.js
+++ weather-app/src/components/search/Search.js
@@ -6,6 +6,7 @@ import { CardMedia, Accordion, AccordionSummary, AccordionDetails, Typography } 
 // Retrieve any API keys or endpoints from another file.
 // This imporves security by keeping sensitive information separate from the main codebase.
 import { API_KEY, LOCATION_API_URL, WEATHER_API_URL, ICON_API_URL } from '../../config/config'
+
 // import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 // import Weather from '../weather/Weather';
 
@@ -20,52 +21,75 @@ const Search = () => {
   const [cityName, setCityName] = useState('');
   const [weather, setWeather] = useState(null);
 
+  // Fetch the weather data from a city based off its longitude and latitude coordinates.
+  // useEffect() will perform a side effect by fetching weather from OpenWeather API.
+  // The weather data is fetched using the OpenWeather API by combining the latitude, longitude, API key, and WEATHER_API_URL.
+  // The useEffect() is called whenever the value of longitude and latitude change (dependencies array).
   useEffect(() => {
     const WEATHER_URL = `${WEATHER_API_URL}?lat=${latitude}&lon=${longitude}&units=imperial&&appid=${API_KEY}`;
     fetchWeather(WEATHER_URL);
   }, [latitude, longitude]);
 
+
+  // Used to submit the data taken in from a form.
+  // In this case, it is taking in a city name from the user and submitted using the "Get Weather" button.
+  // OpenWeather API using the city name, API_KEY and LOCATION_API_URL to get longitude and latitude coordinates of the given city.
+  // 
   const handleSubmit = async (e) => {
     e.preventDefault();
     const LOCATION_URL = `${LOCATION_API_URL}?q=${city}&limit=1&appid=${API_KEY}`
     
-    try { 
+    try {
+      // Make the API call and convert the returned object in json format. 
       const response = await fetch(LOCATION_URL);
       const data  = await response.json();
 
-      if (!data || !data[0]) {
-        // Handle the case where data is undefined or the array is empty
+      // Handle the case where data is undefined or the array is empty
+      if (!data || !data[0]) { 
         console.error(`No data returned for ${city}`);
         return;
       }
 
-      console.log(data);
+      // console.log(data);
       setCityName(data[0].local_names.en);
+
+      // useEffect() is invoked here since the latitude and longitude values are changed.
       setLatitude(data[0].lat);
       setLongitude(data[0].lon);
     }
+    // Error case
     catch (e) { 
       console.log(`Error fetching data: ${e}`);
     }  
   }
 
+  // Fetches the weather of a given URL, in this case it is WEATHER_URL.
+  // OpenWeather API will return the detailed data of a city.
+  // The state of weather is updated using this data.
   const fetchWeather = async (url) => {
     try {
       const response = await fetch(url);
       const weatherData = await response.json();
-      console.log(weatherData);
+      //console.log(weatherData);
       setWeather(weatherData);
     }
+    // Error case
     catch (e){
       console.log(`Error fetching data: ${e}`);
     }
   };
 
+  // A function used to display a 8-day forecast
+  // THe day is determined in the component return section.
   const WeatherCard = ({ day }) => {
 
+    // Convert the UNIX time into a human readable date.
     const date = new Date(day.dt * 1000);
+
+    // OpenWeather API provides icons of the weather
     const ICON_URL = `${ICON_API_URL}/${day.weather[0].icon}.png`
 
+    // Use an accordion from Material UI to display data from the "day" object.
     return (
       <Accordion className='accordion' sx={{background: 'rgb(171,219,227)', margin: '20px', borderRadius: '20px', border: '1px solid black',
       '&:first-of-type': {
@@ -99,12 +123,13 @@ const Search = () => {
         </AccordionDetails>
       </Accordion>
     );
-
   };
 
+  // Search component return
   return (
     <div className='search-container'>
       <Typography variant='h1'>Weather App</Typography>
+      {/* Form used to submit city name */}
       <form 
         onSubmit={handleSubmit}
         style={{
@@ -150,6 +175,8 @@ const Search = () => {
         </Button>
       </form>
 
+      {/* JSX if-statement to check if the weather variable has data in it.
+          It will only render if the statement is true.                    */}
       {weather && weather.current && (
         <div className='forecast'>
           <Typography 
@@ -162,6 +189,9 @@ const Search = () => {
             {cityName}
           </Typography>
 
+          {/* JSX statement is used to map each indiviual day to its own Accordion component.
+              The key is used by React to make this process as effecient as possible.
+              The length of the array is 7 so an 8-day forcast will be returned.            */}
           {weather.daily.map((day, index) => (
 
             <WeatherCard key={index} day={day} />
